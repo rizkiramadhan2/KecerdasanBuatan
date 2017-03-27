3ade0682223b3bc36f1571811df7bad309f2c895
firmankagami <firmanda.nugroho@yahoo.com>
2017-03-27 15:21:51 +0700

    Dikit Lagi. Ada Error dia arraynya riz.

--- src/kecerdasanbuatan/AgenCerdas.java
+++ src/kecerdasanbuatan/AgenCerdas.java
@@ -11,21 +11,39 @@ public class AgenCerdas {
     public String Jenissampah[] = {"debu", "bulu", "kapas", "tisu", "sterofom", "kertas"};
 
     AgenCerdas(int location) {
-        lingkungan = new String[location][3];
+        lingkungan = new String[location][4];
     }
 
-    public void setKondisi(int i, String Nama, String location, String status) {
+    public void setKondisi(int i, String Nama, String location,String status, String sampah) {
         lingkungan[i - 1][0] = Nama;
         lingkungan[i - 1][1] = location;
         lingkungan[i - 1][2] = status;
+        lingkungan[i - 1][3] = sampah;
     }
 
-    public void vacumm(int i, String location, String status) {
+    public void vacumm(int i, String location, String status,String sampah) {
+  
+        String datasampah[]= pisah(sampah);
         if ("kotor".equals(status.toLowerCase())) {
-            System.out.println("----bersih-bersih----");
+              for(int j=0;j<datasampah.length;j++){
+                   boolean ifkotor = false;
+                for(int k=0;k<Jenissampah.length;k++){
+                    if(datasampah[j].equals(Jenissampah[k])){
+                    ifkotor=true;
+                    }else{
+                       ifkotor = false; 
+                    }
+                }
+               if(ifkotor==true){
+                   System.out.println("\tBersih-bersih "+datasampah[j]);
+               }else{
+                   System.out.println("\t"+datasampah[j]+" Bukan sampah");
+               }
+            }
+                      
             status = "bersih";
             lingkungan[i][2] = status;
-            vacumm(i, location, status);
+            vacumm(i, location, status,sampah);
         } else if ("kiri".equals(location.toLowerCase())) {
             System.out.println("geser kanan-->");
         } else if ("kanan".equals(location.toLowerCase())) {
@@ -36,7 +54,7 @@ public class AgenCerdas {
     public void bersihkan() {
         for (int i = 0; i < lingkungan.length; i++) {
             System.out.println("Lokasi" + lingkungan[i][0]);
-            vacumm(i, lingkungan[i][1], lingkungan[i][2]);
+            vacumm(i, lingkungan[i][1], lingkungan[i][2],lingkungan[i][3]);
         }
         cek();
     }
@@ -58,8 +76,8 @@ public class AgenCerdas {
 
     public static void main(String[] args) {
         AgenCerdas a = new AgenCerdas(2);
-        a.setKondisi(1, "Lokasi 1", "kanan", "Debu,Dompet,Gorengan,Kapas,tisu");
-        a.setKondisi(2, "Lokasi 2", "kiri", "Mie,Debu,Tisu,Sterofom,Kertas");
+        a.setKondisi(1, "Lokasi 1", "kanan","kotor","Debu,Dompet,Gorengan,Kapas,tisu");
+        a.setKondisi(2, "Lokasi 2", "kiri","kotor","Mie,Debu,Tisu,Sterofom,Kertas");
         a.bersihkan();
 //
 //        String pis[] = a.pisah("Firmanda Mulyawan Nugroho");
